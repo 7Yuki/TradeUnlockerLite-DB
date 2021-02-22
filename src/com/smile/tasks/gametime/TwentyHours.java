ac19963e737a1dc82e5ca186213e5cd1b8c4a021
DefSmile <notsmile@protonmail.com>
2021-02-22 13:52:00 -0500

    Initial commit

---
+++ src/com/smile/tasks/gametime/TwentyHours.java
@@ -0,0 +1,15 @@
+package com.smile.tasks.gametime;
+
+import org.dreambot.api.script.TaskNode;
+
+public class TwentyHours extends TaskNode {
+    @Override
+    public boolean accept() {
+        return false;
+    }
+
+    @Override
+    public int execute() {
+        return 0;
+    }
+}
