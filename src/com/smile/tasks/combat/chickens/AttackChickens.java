cfda3d5dcb733fe8ed1b89301e94c84cfb9d7fa3
DefSmile <notsmile@protonmail.com>
2021-02-22 17:39:39 -0500

    Fixed accept return statements

--- src/com/smile/tasks/combat/chickens/AttackChickens.java
+++ src/com/smile/tasks/combat/chickens/AttackChickens.java
@@ -1,4 +1,4 @@
-package com.smile.tasks.chickens;
+package com.smile.tasks.combat.chickens;
 
 import com.smile.main;
 import com.smile.settings.NPCTask;
@@ -19,9 +19,9 @@ import static com.smile.settings.Locations.CHICKEN_COOP;
 public class AttackChickens extends TaskNode {
     @Override
     public boolean accept() {
-        return (Skills.getRealLevel(Skill.ATTACK) <= 19)
-                || (Skills.getRealLevel(Skill.STRENGTH) <= 19)
-                || (Skills.getRealLevel(Skill.DEFENCE) <= 19)
+        return (Skills.getRealLevel(Skill.ATTACK) < 19)
+                || Skills.getRealLevel(Skill.STRENGTH) < 19
+                || (Skills.getRealLevel(Skill.DEFENCE) < 19)
                 && !Players.localPlayer().isInCombat();
     }
     @Override
@@ -35,7 +35,7 @@ public class AttackChickens extends TaskNode {
             if (chickens != null) {
                 main.npc = NPCTask.CHICKEN;
                 main.location = CHICKEN_COOP;
-                if (!Players.localPlayer().isInteractedWith() && !Players.localPlayer().isAnimating()) {
+                if (!chickens.isInteractedWith() && !Players.localPlayer().isInCombat()) {
                     if (chickens.interact("Attack")) {
                         main.state = States.ATTACKING;
                         Mouse.moveMouseOutsideScreen();
@@ -45,12 +45,10 @@ public class AttackChickens extends TaskNode {
                         sleepWhile(() -> Players.localPlayer().isAnimating(), chickens::exists,5000,5);
                     }
                 }
-        } else {
-            if (!CHICKEN_COOP.getArea().contains(Players.localPlayer())) {
+        } else if (!CHICKEN_COOP.getArea().contains(Players.localPlayer()) && !Players.localPlayer().isInCombat()) {
                 StatusBar.info("Walking to Chicken coop", false);
                 Walking.walk(CHICKEN_COOP.getArea().getRandomTile());
             }
-        }
         return Calculations.random(1000,1200);
     }
 }
