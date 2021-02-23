5ecd00fc714330a16127cbc8d15afa96f9ab0161
DefSmile <notsmile@protonmail.com>
2021-02-23 12:55:46 -0500

    Initial commit

--- src/com/smile/tasks/combat/worldhop/HopWorld.java
+++ src/com/smile/tasks/combat/worldhop/HopWorld.java
@@ -1,15 +1,42 @@
 package com.smile.tasks.combat.worldhop;
 
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.tabs.Tab;
+import org.dreambot.api.methods.tabs.Tabs;
+import org.dreambot.api.methods.world.World;
+import org.dreambot.api.methods.world.Worlds;
+import org.dreambot.api.methods.worldhopper.WorldHopper;
 import org.dreambot.api.script.TaskNode;
+import java.util.Objects;
 
 public class HopWorld extends TaskNode {
     @Override
     public boolean accept() {
-        return false;
+        return Players.all(Objects::nonNull).toArray().length > 4 && !Players.localPlayer().isInCombat();
+    }
+
+    @Override
+    public int priority() {
+        return 4;
     }
 
     @Override
     public int execute() {
-        return 0;
+        World randomFreeWorld = Worlds.getRandomWorld(w -> w != null && w.isF2P() && w.isNormal());
+        if (WorldHopper.isWorldHopperOpen()) {
+            WorldHopper.hopWorld(randomFreeWorld);
+            if (Worlds.getCurrentWorld() == randomFreeWorld.getWorld()) {
+                if (!Tabs.isOpen(Tab.INVENTORY)) {
+                    Tabs.openWithFKey(Tab.INVENTORY);
+                } else {
+                    sleep(650,750);
+                }
+            }
+        } else {
+            WorldHopper.openWorldHopper();
+            sleep(300,700);
+        }
+        return Calculations.random(1000,1500);
     }
 }
